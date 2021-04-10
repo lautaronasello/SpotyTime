bf7cb7550915187e8960fa715862671beeda43f6
Lautaro Nasello <lautaronasello@gmail.com>
2021-04-09 21:19:00 -0300

    redirecT

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new spotifyWebApi({
-    redirecUri: "http://localhost:3000",
+    redirectUri: "http://localhost:3000",
     clientId: "5e840d136ada47be9bd8479cc2223f1e",
     clientSecret: "790e40bd54204fe3b918d0339cd4d9a1",
     refreshToken,
@@ -32,7 +32,7 @@ app.post("/refresh", (req, res) => {
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new spotifyWebApi({
-    redirecUri: "http://localhost:3000",
+    redirectUri: "http://localhost:3000",
     clientId: "5e840d136ada47be9bd8479cc2223f1e",
     clientSecret: "790e40bd54204fe3b918d0339cd4d9a1",
   });
