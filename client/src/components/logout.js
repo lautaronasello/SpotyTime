bf746dbd20467d88ea04938aa7d931145f523b7d
Lautaro Nasello <lautaronasello@gmail.com>
2021-04-20 15:30:11 -0300

    just need to deploy

--- client/src/components/logout.js
+++ client/src/components/logout.js
@@ -4,21 +4,16 @@ import { Button } from "react-bootstrap";
 const url = "https://accounts.spotify.com/en/logout";
 var logout;
 
-function popUp() {
+function finishLogout() {
   logout = window.open(
     url,
     "Spotify Logout",
     "width=700,height=500,top=40,left=40"
   );
   setTimeout(() => logout.close(), 1000);
+  setTimeout(() => (window.location.href = "/"), 1500);
 }
 
-function finishLogout() {
-  popUp();
-  if (popUp) {
-    setTimeout(() => (window.location.href = "/"), 2000);
-  }
-}
 export default function Logout() {
   return (
     <Button onClick={finishLogout} variant="outline-success">
